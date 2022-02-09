4cb6993ed3ef622e93d6ffb1d4cb947027fb08d7
Vasil Filipov <vakko_vakko@abv.bg>
2022-02-09 16:32:27 +0200

    Push a working solution

--- include/median.hxx
+++ include/median.hxx
@@ -1,6 +1,17 @@
 #ifndef __MEDIAN_HXX
 #define __MEDIAN_HXX
 
-int add(int a, int b);
+#include <queue>
+#include <vector>
+
+class MedianFinder {
+public:
+    MedianFinder();
+    ~MedianFinder();
+    std::priority_queue<double> maxHeap; //lower half of the numbers
+    std::priority_queue<double, std::vector<double>, std::greater<double>> minHeap; //upper half
+    void addNum(double num);
+    double findMedian();
+};
 
 #endif // __MEDIAN_HXX
--- src/median.cpp
+++ src/median.cpp
@@ -1,6 +1,66 @@
 #include "median.hxx"
+#include <cassert>
 
-int add(int a, int b)
+MedianFinder::MedianFinder() {};
+MedianFinder::~MedianFinder() {};
+
+void MedianFinder::addNum(double num)
 {
-    return a + b;
+    if (maxHeap.size()==minHeap.size()) {
+        maxHeap.push(num);
+        minHeap.push(maxHeap.top());
+        maxHeap.pop();
+    }
+    else {
+        minHeap.push(num);
+        maxHeap.push(minHeap.top());
+        minHeap.pop();
+    }
+}
+
+double MedianFinder::findMedian() 
+{        
+    if (maxHeap.size()==minHeap.size()) 
+        return (minHeap.top() + maxHeap.top()) / 2.0;
+    else
+        return minHeap.top();
+}
+
+
+int main() {
+    MedianFinder* medianFinder = new MedianFinder();
+    medianFinder->addNum(1);
+    medianFinder->addNum(2);
+    assert (medianFinder->findMedian() == 1.5);
+    medianFinder->addNum(3);
+    assert (medianFinder->findMedian() == 2.0);
+
+    delete medianFinder;
+    //new(&medianFinder) MedianFinder();
+    medianFinder = new MedianFinder();
+    
+    medianFinder->addNum(6);
+    assert (medianFinder->findMedian() == 6.0);
+    medianFinder->addNum(10);
+    assert (medianFinder->findMedian() == 8.0);
+    medianFinder->addNum(2);
+    assert (medianFinder->findMedian() == 6.0);
+    medianFinder->addNum(6);
+    assert (medianFinder->findMedian() == 6.0);
+    medianFinder->addNum(5);
+    assert (medianFinder->findMedian() == 6.0);
+    medianFinder->addNum(0);
+    assert (medianFinder->findMedian() == 5.5);
+    medianFinder->addNum(6);
+    assert (medianFinder->findMedian() == 6.0);
+    medianFinder->addNum(3);
+    assert (medianFinder->findMedian() == 5.5);
+    medianFinder->addNum(1);
+    assert (medianFinder->findMedian() == 5.0);
+    medianFinder->addNum(0);
+    assert (medianFinder->findMedian() == 4.0);
+    medianFinder->addNum(0);
+    assert (medianFinder->findMedian() == 3.0);
+    
+    return 0;
 }
